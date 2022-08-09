ba57cdc92fca37c8abcf9c7519f609d0d7b7ae81
brunanovais <93659717+brunanovais@users.noreply.github.com>
2022-08-08 23:25:47 -0300

    entendendo funções

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-const inputs = document.querySelectorAll("#input");
+const inputs = document.querySelectorAll("#input")
 
 const handLeFocus = ({ target }) => {
     const span = target.previousElementSibling;
     span.classList.add("span_ativo");
+    console.log(target)
 }
 
-inputs.forEach((input) => input.addEventListener("focus", handLeFocus));
+ const handLeFocusOut = ({ target }) => {
+    const span = target.previousElementSibling;
+    span.classList.remove("span_ativo");
+}
+    
+inputs.forEach((i) => i.addEventListener("focus", handLeFocus));
+
+inputs.forEach((input) => input.addEventListener("focusout", handLeFocusOut));
